205dac79bd93fac3b28ea8f17b8c410adfda73ed
Nick Tkachov <ntkachov@gmail.com>
2015-03-01 17:16:01 -0600

    Added card interface to blog

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,6 @@
 $(function(){
+    var stripHTML = new RegExp('<.*?>', "g");
+
     $.ajax({
         url: "blog.json",
         success: function(data){
@@ -19,7 +21,24 @@ $(function(){
     function buildBlog(blog){
         for(var post in blog) {
                 post = blog[post];
-              $("#blogContent").append(post["title"]);
+                var blurb = post.blurb.replace(stripHTML,"") 
+                var postElement = $('<li class="blogTitle card"><h1>'
+                  + post.title + '</h1><div id="blurb">'
+                  + blurb + '</div><div id="body" class="postBody">'
+                  + post.body + '</div></li>');
+                postElement.find("#body").hide();
+                postElement.click(function(){
+                    var clickTarget = $(this);
+                    clickTarget.siblings().toggle(250);
+                    clickTarget.find("#body").toggle(250 , function(){
+                    $('html, body').animate({
+                                scrollTop: clickTarget.offset().top
+                                    }, 250);
+                    });
+                });
+
+              $("#blogContent").append(postElement);
+
         }
     }
 });
